6e51365c87336ae859eecfb4bcb77eab408b2a7b
Quang Le <lenhuquang99@gmail.com>
2020-04-26 00:20:56 +0300

    Add: use ElementUI, ChartJs

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+```
+npm i element-ui -S
+npm install babel-plugin-component -D
+npm i -S chart.js
+npm install --save-dev webpack-bundle-analyzer
+```
--- src/main.js
+++ src/main.js
@@ -1,7 +1,35 @@
 import Vue from 'vue';
+
+import {
+  Col, Row, Card, Form, FormItem,
+  Button, Select, Option, RadioButton, RadioGroup,
+  InputNumber, Slider, ColorPicker, Popover,
+} from 'element-ui';
+
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+locale.use(lang);
+Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 };
+
+Vue.use(Col);
+Vue.use(Row);
+Vue.use(Card);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Button);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(RadioButton);
+Vue.use(RadioGroup);
+Vue.use(InputNumber);
+Vue.use(Slider);
+Vue.use(ColorPicker);
+Vue.use(Popover);
 
 Vue.config.productionTip = false;
 
